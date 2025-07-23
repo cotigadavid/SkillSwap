c4b528ba20e95ea54c719c9e46b4c36876f6af36
Gabriel <gabrielstefan04@gmail.com>
2025-07-23 14:42:37 +0300

    skill post and list component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import AuthWindow from './components/AuthWindow';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import SkillAdvertisement from './components/SkillAdvertisement'
+import SkillList from './components/SkillList';
 
 function App() {
   return (
     <div className='main'>
-      <AuthWindow/>
+      {/*<AuthWindow/>*/}
+      <SkillList/>
     </div>
+    
   );
 }
 
