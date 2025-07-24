1754b273a26b1c1c4c9a320cb6811f2717923c9f
david <cotigadavid@gmail.com>
2025-07-24 10:21:21 +0300

    minor things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import AuthWindow from './components/AuthWindow';
 import UserProfile from './components/UserProfile';
+import AddSkill from './components/AddSkill';
 
 function App() {
   return (
     <div className='main'>
-      <UserProfile/>
+      <AddSkill/>
     </div>
   );
 }
