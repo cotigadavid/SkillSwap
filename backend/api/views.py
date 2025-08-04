98f279eaa2172a466cc87df5f7966d0621a3edbb
Gabriel <gabrielstefan04@gmail.com>
2025-08-05 02:14:18 +0300

    skill request feature finished

--- backend/api/views.py
+++ backend/api/views.py
@@ -141,6 +141,11 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
