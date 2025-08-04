7efb6e043ff426b21eeacf509a9f1ffde79181a4
david <cotigadavid@gmail.com>
2025-08-04 08:52:17 +0300

    init tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import SkillList from './components/SkillList';
 import UserProfile from './components/UserProfile';
 import AdInfo from './components/AdInfo';
@@ -11,15 +11,18 @@ import Conversations from './components/Conversations';
 import AddProfilePicture from './components/AddProfilePhoto';
 import UserEditCard from './components/UserEditCard';
 import OtherProfile from './components/OtherProfile';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className='main'>
       <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/skills/:id" element={<AdInfo />} />
         <Route path="/auth" element={<AuthWindow/>}/>
-        <Route path="/" element={<Conversations />} />
+        <Route path="/conv" element={<Conversations />} />
         <Route path="/conv/:convId" element={<ChatWindow />} />
-        <Route path="/add" element={<UserEditCard/>}/>
+        <Route path="/my-profile" element={<UserProfile />} />
         <Route path="/profile/:userId" element={<OtherProfile/>}/>
       </Routes>
     </div>
